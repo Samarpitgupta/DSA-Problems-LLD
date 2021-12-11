1f605de390a20d03a5ce4baa1c59e346d192c193
samarpit <samarguptag@gmail.com>
2021-12-11 22:00:45 +0530

    update

--- DSA-ProblemSolving/src/leetcode/ds1/PascalTriangle.java
+++ DSA-ProblemSolving/src/leetcode/ds1/PascalTriangle.java
@@ -16,8 +16,7 @@ public class PascalTriangle {
 		List<List<Integer>> lists = new ArrayList<List<Integer>>(numRows);
 		for (int i = 0; i < numRows; i++) {
 			List<Integer> list = new ArrayList<>(Collections.nCopies(i + 1, 1));
-			System.out.println(list.size());
-			// make by default zero & update the middle values
+			// make by default 1 & update the middle values
 			for (int j = 1; j < i; j++) { // current row -> i , previous row i-1
 				list.add(j, lists.get(i - 1).get(j) + lists.get(i - 1).get(j - 1)); // add values at particular index
 				list.remove(list.size() - 1); // remove last element from the list due last step element addition
