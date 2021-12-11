fdc05ee9dc3296d88a3b8b378fe091276a3ca7b0
samarpit <samarguptag@gmail.com>
2021-12-11 21:53:34 +0530

    move zeros

--- DSA-ProblemSolving/src/leetcode/algo/MoveZeros.java
+++ DSA-ProblemSolving/src/leetcode/algo/MoveZeros.java
@@ -1,10 +1,24 @@
 package leetcode.algo;
 
+import java.util.Arrays;
+
 public class MoveZeros {
 
 	public static void main(String arg) {
-		
-		
+
+	}
+
+	public static void moveAllZeroToEnd(int[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				int temp = arr[i];
+				arr[i] = arr[count];
+				arr[count] = temp;
+				count++;
+			}
+		}
+		System.out.print(Arrays.toString(arr));// ans in not coming correct // needs to do dry run and fix
 	}
 
 }
