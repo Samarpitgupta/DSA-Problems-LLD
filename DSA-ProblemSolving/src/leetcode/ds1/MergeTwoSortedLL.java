dab49ab5588fcf5a74a5b55636255b3c034d30b0
samarpit <samarguptag@gmail.com>
2021-12-14 22:46:45 +0530

    day7 leetcode problems

---
+++ DSA-ProblemSolving/src/leetcode/ds1/MergeTwoSortedLL.java
@@ -0,0 +1,69 @@
+package leetcode.ds1;
+
+public class MergeTwoSortedLL {
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null) {
+			return list2;
+		} else if (list2 == null) {
+			return list1;
+		}
+
+		ListNode current1 = list1;
+		ListNode current2 = list2;
+		ListNode newHead = null;
+		ListNode newTail = null;
+
+		if (current1.val <= current2.val) {
+			newHead = new ListNode(current1.val);
+			newTail = newHead;
+			current1 = current1.next;
+		} else {
+			newHead = new ListNode(current2.val);
+			newTail = newHead;
+			current2 = current2.next;
+		}
+
+		while (current1 != null && current2 != null) {
+			if (current1.val <= current2.val) {
+				newTail.next = new ListNode(current1.val);
+				current1 = current1.next;
+			} else {
+				newTail.next = new ListNode(current2.val);
+				current2 = current2.next;
+			}
+			newTail = newTail.next;
+		}
+
+		while (current1 != null) {
+			newTail.next = new ListNode(current1.val);
+			current1 = current1.next;
+			newTail = newTail.next;
+		}
+
+		while (current2 != null) {
+			newTail.next = new ListNode(current2.val);
+			current2 = current2.next;
+			newTail = newTail.next;
+		}
+
+		return newHead;
+
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
---
+++ DSA-ProblemSolving/src/leetcode/ds1/RemoveNodeLL.java
@@ -0,0 +1,26 @@
+package leetcode.ds1;
+
+public class RemoveNodeLL {
+
+	public ListNode removeElements(ListNode head, int val) {
+		if (head == null)
+			return null;
+		ListNode cur = head;
+		ListNode dummy = new ListNode();
+		ListNode prev = dummy;
+		prev.next = head;
+		while (cur != null) {
+			if (cur.val == val) {
+				prev.next = cur.next;
+				cur = cur.next;
+
+			} else {
+				cur = cur.next;
+				prev = prev.next;
+			}
+		}
+		return dummy.next;
+
+	}
+
+}
