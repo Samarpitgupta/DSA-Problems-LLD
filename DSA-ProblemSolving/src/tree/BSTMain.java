c1fd9a8b42afec3813ebc95167eb9cf5182ecc2f
samarpit <samarguptag@gmail.com>
2021-12-20 23:00:29 +0530

    BST

--- DSA-ProblemSolving/src/tree/BSTMain.java
+++ DSA-ProblemSolving/src/tree/BSTMain.java
@@ -3,8 +3,21 @@ package tree;
 public class BSTMain {
 
 	public static void main(String args[]) {
-
 	}
-	
-	
+
+	public TreeNode searchBST(TreeNode root, int val) {
+		if (root == null)
+			return null;
+
+		if (root.val == val)
+			return root;
+		else if (root.val > val)
+			return searchBST(root.left, val);
+		else if (root.val < val)
+			return searchBST(root.right, val);
+
+		return null;
+	}
+
+
 }
