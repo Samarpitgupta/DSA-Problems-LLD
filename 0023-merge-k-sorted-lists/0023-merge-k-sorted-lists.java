8851fe10eec3c28d5523976b5f29c54eaac21dbc
Samarpit Gupta <samarguptag@gmail.com>
2024-06-11 20:00:57 +0530

    Time: 4 ms (74.61%), Space: 44.2 MB (59.53%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        
+        // Add the heads of all k lists to the min-heap
+        for (ListNode node : lists) {
+            if (node != null)
+                minHeap.offer(node);
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        
+        //  while the min-heap is not empty
+        while (!minHeap.isEmpty()) {
+            // Extract the minimum node from the heap
+            ListNode minNode = minHeap.poll();
+            current.next = minNode;
+            current = current.next;
+            
+            // Move the pointer of the extracted node to its next node in its list
+            if (minNode.next != null) {
+                minHeap.offer(minNode.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
