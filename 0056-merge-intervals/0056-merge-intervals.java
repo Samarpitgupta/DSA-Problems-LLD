951beb118f30db9542a0e36d266021c236a97a7a
Samarpit Gupta <samarguptag@gmail.com>
2024-06-22 22:37:39 +0530

    Time: 7 ms (97.07%), Space: 46.2 MB (74.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) return intervals;
+        if (intervals.length == 0) {
+            return new int[0][];
+        }
 
-        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+        // Sort the intervals based on the starting time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        List < int[] > output_arr = new ArrayList < > ();
-        int[] current_interval = intervals[0];
-        output_arr.add(current_interval);
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
 
-        for (int[] interval: intervals) {
-            int current_begin = current_interval[0];
-            int current_end = current_interval[1];
-            int next_begin = interval[0];
-            int next_end = interval[1];
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
 
-            if (current_end >= next_begin) {
-                current_interval[1] = Math.max(current_end, next_end);
+            if (currentEnd >= nextStart) {
+                // Overlapping intervals, merge them
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
             } else {
-                current_interval = interval;
-                output_arr.add(current_interval);
+                // Non-overlapping interval, add the previous interval and move to the next
+                currentInterval = interval;
+                merged.add(currentInterval);
             }
         }
-        return output_arr.toArray(new int[output_arr.size()][]);
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
